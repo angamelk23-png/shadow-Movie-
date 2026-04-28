f52ff69edee8f94db672a81eda2e8afae5b25c57
angamelk23-png <angamelk23@gmail.com>
2026-04-28 21:33:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,15 +2,14 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-BOT_TOKEN=os.getenv('BOT_TOKEN')
-TMDB_KEY=os.getenv('TMDB_KEY')
-MONGO_URI=os.getenv('MONGO_URI')
-BOT_USERNAME=os.getenv('BOT_USERNAME')
-OWNER_ID=int(os.getenv('OWNER_ID'))
+BOT_TOKEN=os.getenv('8746836615:AAGohlbiEskI_mIO5F1JGndhYf0CvN_BPRY')
+TMDB_KEY=os.getenv('af43e0289f758ede6ae31d20cc541776')
+MONGO_URI=os.getenv('mongodb+srv://chamadithbuvindu_db_user:6kQaqQ0XSFPax9SO@cluster0.ywevrcf.mongodb.net/?appName=Cluster0')
+BOT_USERNAME=os.getenv('@Alexia_x1bot')
+OWNER_ID=int(os.getenv('8458894963'))
 REDIS_URL=os.getenv('REDIS_URL')
-UPDATE_CHANNEL=os.getenv('UPDATE_CHANNEL')
+UPDATE_CHANNEL=os.getenv('@SHADOW_Movie23')
 
 SOURCE_CHANNELS=[
--1001111111111,
--1002222222222,
+-1003994779583
 ]
