0a64fd61b862274786c01917124aab69600c7469
angamelk23-png <angamelk23@gmail.com>
2026-04-28 22:04:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ BOT_TOKEN=os.getenv('8746836615:AAGohlbiEskI_mIO5F1JGndhYf0CvN_BPRY')
 TMDB_KEY=os.getenv('af43e0289f758ede6ae31d20cc541776')
 MONGO_URI=os.getenv('mongodb+srv://chamadithbuvindu_db_user:6kQaqQ0XSFPax9SO@cluster0.ywevrcf.mongodb.net/?appName=Cluster0')
 BOT_USERNAME=os.getenv('@Alexia_x1bot')
-OWNER_ID=int(os.getenv('8458894963'))
+OWNER_ID=int(os.getenv(' 8458894963 '))
 REDIS_URL=os.getenv('REDIS_URL')
 UPDATE_CHANNEL=os.getenv('@SHADOW_Movie23')
 
